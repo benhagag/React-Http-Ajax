17df67d7d4f5c7c471068bcf35a50dbb719516fc
Ben Hagag <benbenhagag@google.com>
2020-11-17 14:26:55 +0200

    Adding interceptors excute code gllobaly

--- src/index.js
+++ src/index.js
@@ -3,6 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+/**
+ * Config the interceptors in request config.
+ * 
+ * Axios interceptors are functions that Axios calls for every request.
+ * You can use interceptors to transform the request before Axios sends it, 
+ * or transform the response before Axios returns the response to your code.
+ */
+axios.interceptors.request.use(request => {
+    console.log(request);
+    // Here We can edit the request config (add Headers, Chnage them, etc...)
+    // We have to return the request
+    return request;
+},error => {
+    console.log(error);
+    return Promise.reject(error);
+});
+
+axios.interceptors.response.use(response => {
+    console.log(response);
+    return response;
+},error => {
+    console.log(error);
+    return Promise.reject(error);
+});
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
